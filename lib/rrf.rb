68d0acbd57864d4fff977db4f93f3d994ad9f15c
Chris Petersen <chris@petersen.io>
2024-07-15 13:53:07 -0700

    Initial version of the RRF gem

---
+++ lib/rrf.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "rrf/version"
+
+module Rrf
+  class Error < StandardError; end
+
+  def self.placeholder
+    "This is a placeholder for the Reciprocal Rank Fusion gem."
+  end
+end
---
+++ spec/rrf_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rrf do
+  it "has a version number" do
+    expect(Rrf::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
